d4c7a0e09122802b7dd2235412d25433e75b3cd8
Warren Gay <ve3wwg@gmail.com>
2013-12-03 19:36:28 -0500

    No more need for dummy types

--- 0005.ads
+++ 0005.ads
@@ -13,21 +13,3 @@
 with System;
 
 package Posix is
-
-    -- These types are temporary and for testing.. for the moment..
-    type int_t is range -2**31 .. 2**31-1;
-
-    type errno_t is new int_t range 0 .. 2**31-1;
-    type fcntl_t is new int_t;
-    type whence_t is new int_t;
-    type mntflags_t is new int_t;
-
-    type amode_t is range -2**16 .. 2**16-1;
-    type mode_t is new int_t;
-    type pid_t is new int_t;
-    type time_t is new int_t;
-    type dev_t is new int_t;
-    type uid_t is new int_t;
-    type gid_t is new int_t;
-    type offset_t is new int_t;
-    type uint_t is new int_t;
