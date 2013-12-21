3786218cd2366ef4a62fbc57336f0720aed05866
Warren Gay <ve3wwg@gmail.com>
2013-12-20 20:50:34 -0500

    Added for type'Size clause generation

--- systypes.cpp
+++ systypes.cpp
@@ -43,6 +43,15 @@ emit_sys_types() {
 				<< "2**" << (node.size*8-1) << "-1;\n";
 		}
 	}
+
+	ads << "\n";
+
+	for ( auto it=config.sys_types.info.begin(); it != config.sys_types.info.end(); ++it ) {
+		const std::string& name = it->first;
+		s_config::s_sys_types::s_sys_type& node = it->second;
+
+		ads << "    for " << name << "'Size use " << node.size*8 << ";\n";
+	}
 }
 
 // End systypes.cpp
