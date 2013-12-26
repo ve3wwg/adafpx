57c384641d3d9c20a1d14e2dffc428d86d66d739
Warren Gay <ve3wwg@gmail.com>
2013-12-25 22:26:55 -0500

    Added more API

--- 0600.ads
+++ 0600.ads
@@ -2,3 +2,5 @@
    type s_iovec_array is array(size_t range <>) of s_iovec;
    type argv_array is array(Natural range <>) of System.Address;
    type fd_array is array(Natural range <>) of fd_t;
+   type gid_array is array(Natural range <>) of gid_t;
+
