183eff1a858dfde53a46b40d35e9caee2add8dea
Warren Gay <ve3wwg@gmail.com>
2013-12-27 22:19:55 -0500

    Added syslog test

--- atest.adb
+++ atest.adb
@@ -2,8 +2,12 @@ with Posix;
 use Posix;
 
 procedure ATest is
-    Error : errno_t;
+   Error : errno_t;
 begin
-    Close(5,Error);
-    pragma Unreferenced(Error);
+   Close(5,Error);
+   pragma Unreferenced(Error);
+
+   Openlog("Ada Test",LOG_PID or LOG_CONS,LOG_LOCAL7);
+   Syslog(5,"Testing from atest.adb");
+   Closelog;
 end ATest;
