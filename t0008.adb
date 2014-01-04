30f2f3dba7d673925aac8e235faf15b1eb475d7f
Warren Gay <ve3wwg@gmail.com>
2014-01-04 10:24:50 -0500

    Wait_Pid test case t0008

---
+++ t0008.adb
@@ -0,0 +1,71 @@
+-- t0008.adb - Sat Jan  4 10:20:17 2014
+--
+-- (c) Warren W. Gay VE3WWG  ve3wwg@gmail.com
+--
+-- Protected under the following license:
+-- GNU LESSER GENERAL PUBLIC LICENSE Version 2.1, February 1999
+
+with Ada.Text_IO;
+
+with Posix;
+use Posix;
+
+procedure T0008 is
+   use Ada.Text_IO;
+
+   Parent : pid_t := -1;
+   Child :  pid_t := -1;
+   P_PID :  pid_t := -1;
+   Error :  errno_t;
+begin
+
+   Put_Line("Test 0008 - Fork/Wait_Pid/Getpid/Getppid/Kill");
+
+   Getpid(Parent);
+
+   Put_Line("Parent: My PID is " & pid_t'Image(Parent));
+
+   Fork(Child,Error);
+
+   pragma Assert(Error = 0);
+   
+   if Child = 0 then
+      ----------------------------------------------------------------
+      -- This is the child process running
+      ----------------------------------------------------------------
+      Getpid(Child);
+      Getppid(P_PID);
+      Put_Line("Child: My PID is " & pid_t'Image(Child) & " and my Parent is " & pid_t'Image(P_PID));
+      pragma Assert(P_PID = Parent);
+      Kill(Child,SIGINT,Error);
+      pragma Assert(Error = 0);
+      loop
+         P_PID := Child;   -- Loop until terminated
+      end loop;
+   else
+      ----------------------------------------------------------------
+      -- This is the parent process running
+      ----------------------------------------------------------------
+      Put_Line("Parent: My child PID is " & pid_t'Image(Child));
+
+      declare
+         Status : int_t := -1;
+         Sig :    sig_t := -1;
+      begin
+
+         Wait_Pid(Child,0,Status,Error);
+         pragma Assert(Error = 0);
+         pragma Assert(WIFSIGNALED(Status) = True);
+
+         Sig := WTERMSIG(Status);
+         pragma Assert(Sig = SIGINT);
+
+         Put_Line("Parent: My child process has been signaled with SIGINT");
+      end;
+
+   end if;
+
+   Put_Line("Test 0008 Passed.");
+
+end T0008;
+
