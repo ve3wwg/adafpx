5e9496aa0fc9b44e60cb0f700135e453b550c055
Warren Gay <ve3wwg@gmail.com>
2014-01-05 20:16:18 -0500

    Defined fixed array s_timeval_array2

--- 0690.ads
+++ 0690.ads
@@ -3,4 +3,6 @@
    type argv_array is array(Natural range <>) of System.Address;
    type fd_array is array(Natural range <>) of fd_t;
    type gid_array is array(Natural range <>) of gid_t;
+   type s_timeval_array is array(Natural range <>) of s_timeval;
+   type s_timeval_array2 is array(Natural range 1..2) of s_timeval;
 
