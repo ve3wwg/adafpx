d5f8a073b64d7eda78d4845f1b7fc514e21b095f
Warren Gay <ve3wwg@gmail.com>
2014-01-07 20:34:46 -0500

    Completed test

--- t0018.adb
+++ t0018.adb
@@ -17,13 +17,12 @@ procedure T0018 is
    My_Uid :    constant uid_t := Geteuid;
    My_Gid :    constant gid_t := Getegid;
    Fd :        fd_t;
-   Old_Mask :  mode_t;
    S :         s_stat;
    Error :     errno_t;
-   Times :     s_utimbuf;
+   Confirms :  Natural := 0;
 begin
 
-   Put_Line("Test 0018 - Chmod/FChmod");
+   Put_Line("Test 0018 - Chown/FChown");
 
    pragma Warnings(Off);
    Unlink(Path,Error);           -- Ignore errors
@@ -41,36 +40,50 @@ begin
    pragma Assert(S.st_uid = My_Uid);
    pragma Assert(S.st_gid = My_Gid);
 
-   Chown(Path,0,0,Error);
-   pragma Assert(Error = 0);
-   
-   Stat(Path,S,Error);
-   pragma Assert(Error = 0);
-   pragma Assert(S.st_uid = 1);
-   pragma Assert(S.st_gid = 1);
+   Chown(Path,1,2,Error);
+   if Error = 0 then
+      Stat(Path,S,Error);
+      pragma Assert(Error = 0);
+      pragma Assert(S.st_uid = 1);
+      pragma Assert(S.st_gid = 2);
+      Confirms := Confirms + 1;
+   else
+      pragma Assert(Error = EPERM);
+      null;
+   end if;   
 
    Unlink(Path,Error);
    pragma Assert(Error = 0);
-
 
    Create(Path,8#777#,Fd,Error);
    pragma Assert(Error = 0);
    pragma Assert(Fd >= 0);
 
    FChown(Fd,2,3,Error);
-   pragma Assert(Error = 0);
 
-   Stat(Path,S,Error);
-   pragma Assert(Error = 0);
-   pragma Assert(S.st_uid = 2);
-   pragma Assert(S.st_gid = 3);
+   if Error = 0 then
+      Stat(Path,S,Error);
+      pragma Assert(Error = 0);
+      pragma Assert(S.st_uid = 2);
+      pragma Assert(S.st_gid = 3);
+      Confirms := Confirms + 1;
+   else
+      pragma Assert(Error = EPERM);
+      null;
+   end if;
 
    Close(Fd,Error);
    pragma Assert(Error = 0);
 
    Unlink(Path,Error);
    pragma Assert(Error = 0);
+   
+   Put("Test 0018 Passed");
 
-   Put_Line("Test 0018 Passed.");
+   if Confirms = 2 then
+      Put_Line(" and confirmed.");
+   else
+      Put_Line(" but must run as root to confirm.");
+   end if;
 
 end T0018;
