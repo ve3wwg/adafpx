9b9dd7ce580ba3bd6406ed36189d7bd96b65ce4c
Warren Gay <ve3wwg@gmail.com>
2014-01-11 18:13:02 -0500

    Completed test 23

--- t0023.adb
+++ t0023.adb
@@ -13,42 +13,89 @@ use Posix;
 procedure T0023 is
    use Ada.Text_IO;    
 
-   Path :            constant String := "Makefile";
+   Path :            constant String := "Test_0023";
    Fd :              fd_t := -1;
    S :               s_flock;
+   PID :             constant pid_t := Getpid;
+   Child :           pid_t := -1;
+   Count :           Natural := 0;
    Error :           errno_t;
-   -- <case name="F_GETLK"/>
-   -- <case name="F_SETLK"/>
-   -- <case name="F_SETLKW"/>
 begin
 
    Put_Line("Test 0023 - Fcntl(F_GET/SETFD/F_GET/SETFL)");
 
-   Open(Path,O_RDONLY,Fd,Error);
+   pragma Warnings(Off);
+   Unlink(Path,Error);     -- Ignore error
+   pragma Warnings(On);
+
+   Create(Path,8#660#,Fd,Error);
    pragma Assert(Error = 0);
    pragma Assert(Fd >= 0);
    
+   Write(Fd,To_uchar_array(Path),Count,Error);
+   pragma Assert(Error = 0);
+   pragma Assert(Count = Path'Length);
+
+   S.l_type := F_WRLCK;
+   S.l_whence := 0;
+   S.l_start := 0;
+   S.l_len := 0;     
+
    Fcntl(Fd,F_GETLK,S,Error);
    pragma Assert(Error = 0);
    pragma Assert(S.l_type = F_UNLCK);
 
-   S.l_type := F_WRLCK;                -- Write lock
-   S.l_start := 1;                     -- Starting at 1
-   S.l_len := 0;                       -- To end file
-   S.l_whence := short_t(SEEK_SET);    -- Use absolute offsets
-
-   Fcntl(Fd,F_SETLK,S,Error);
+   Fork(Child,Error);
    pragma Assert(Error = 0);
 
-   S.l_type := F_UNLCK;
+   if Child = 0 then
+      -- Child process
+
+      S.l_type := F_WRLCK;                -- Read lock
+      S.l_start := 1;                     -- Starting at 1
+      S.l_len := 0;                       -- To end file
+      S.l_whence := short_t(SEEK_SET);    -- Use absolute offsets
+      S.l_pid := Getpid;
+
+      Fcntl(Fd,F_SETLK,S,Error);
+      pragma Assert(Error = 0);
+
+      loop
+         Kill(PID,0,Error);
+         exit when Error = ESRCH;
+         delay 1.0;
+      end loop;
+
+      Close(Fd,Error);
+      pragma Assert(Error = 0);
+
+      Sys_Exit(0);
+   end if;
+
+   -------------------------------------------------------------------
+   -- Parent process
+   -------------------------------------------------------------------
+
+   delay 2.0;
+
+   S.l_type := F_WRLCK;                -- Read lock
    S.l_start := 0;
-   S.l_len := 99;
+   S.l_len := 0;                       -- To end file
+   S.l_whence := short_t(SEEK_SET);    -- Use absolute offsets
+   S.l_pid := 0;
 
    Fcntl(Fd,F_GETLK,S,Error);
    pragma Assert(Error = 0);
    pragma Assert(S.l_type = F_WRLCK);
+   pragma Assert(S.l_start = 1);
+   pragma Assert(S.l_len = 0);
+   pragma Assert(S.l_whence = short_t(SEEK_SET));
+   pragma Assert(S.l_pid = Child);
 
    Close(Fd,Error);
+   pragma Assert(Error = 0);
+
+   Unlink(Path,Error);
    pragma Assert(Error = 0);
 
    Put_Line("Test 0023 Passed.");
