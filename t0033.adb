1ee0b78c31d947a037731b01e411e5e00ae10622
Warren Gay <ve3wwg@gmail.com>
2014-01-14 21:14:53 -0500

    Eliminated useless Time Zone param, + added Tzset test

--- t0033.adb
+++ t0033.adb
@@ -14,22 +14,16 @@ procedure T0033 is
    use Ada.Text_IO;    
 
    Time_Date :    s_timeval;
-   Time_Zone :    s_timezone;
    Error :        errno_t;
 begin
 
-   Put_Line("Test 0033 - Gettimeofday");
+   Put_Line("Test 0033 - Tzset/Gettimeofday");
 
-   Gettimeofday(Time_Date,Time_Zone,Error);
+   Tzset;
+
+   Gettimeofday(Time_Date,Error);
    pragma Assert(Error = 0);
    pragma Assert(Time_Date.tv_sec >= 1389750474);
-
-   Put_Line("Time Zone," & int64_t'Image(int64_t(Time_Zone.tz_minuteswest))& " minutes west");
-   if Time_Zone.tz_dsttime /= 0 then
-      Put_Line("Is currently observing daylight savings time.");
-   else
-      Put_Line("Is NOT currently observing daylight savings time.");
-   end if;
 
    Put_Line("Test 0033 Passed.");
 
