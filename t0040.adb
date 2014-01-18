975537b2e225d46454a6318e51a102652db3cecf
Warren Gay <ve3wwg@gmail.com>
2014-01-17 20:42:27 -0500

    Test 40 uname

---
+++ t0040.adb
@@ -0,0 +1,33 @@
+-- t0040.adb - Fri Jan 17 20:36:08 2014
+--
+-- (c) Warren W. Gay VE3WWG  ve3wwg@gmail.com
+--
+-- Protected under the following license:
+-- GNU LESSER GENERAL PUBLIC LICENSE Version 2.1, February 1999
+
+with Ada.Text_IO;
+
+with Posix;
+use Posix;
+
+procedure T0040 is
+   use Ada.Text_IO;    
+
+   Names :  s_utsname;
+   Error :  errno_t;
+begin
+
+   Put_Line("Test 0040 - Syslog");
+
+   UName(Names,Error);
+   pragma Assert(Error = 0);
+
+   Put_Line("Sysname:  '" & Ada_String(Names.sysname) & "'");
+   Put_Line("Nodename: '" & Ada_String(Names.Nodename) & "'");
+   Put_Line("Release:  '" & Ada_String(Names.Release) & "'");
+   Put_Line("Version:  '" & Ada_String(Names.Version) & "'");
+   Put_Line("Machine:  '" & Ada_String(Names.Machine) & "'");
+
+   Put_Line("Test 0040 Passed.");
+
+end T0040;
