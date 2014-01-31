8c9d994888841593fbd58b4bc90dc3ae336ce8c5
Warren Gay <ve3wwg@gmail.com>
2014-01-30 21:21:40 -0500

    Added Pos_Natural(ssize_t)

--- 0005.adb
+++ 0005.adb
@@ -298,6 +298,17 @@ package body Posix is
 
    pragma Inline(Pos_Natural);
 
+   function Pos_Natural(Status: ssize_t) return Natural is
+   begin
+      if Status >= 0 then
+         return Natural(Status);
+      else
+         return 0;
+      end if;
+   end Pos_Natural;
+
+   pragma Inline(Pos_Natural);
+
    function Argv_Length(Argvs: String) return Natural is
       Count : Natural := 0;   
    begin
