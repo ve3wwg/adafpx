585a8688c64169139f5d03824c3c3f573ac0a7b6
Warren Gay <ve3wwg@gmail.com>
2014-12-27 14:38:01 -0500

    Added C union u_sockaddr for use in getpeername/getsockname

--- 0690.ads
+++ 0690.ads
@@ -5,3 +5,18 @@
    type gid_array is array(Natural range <>) of gid_t;
    type s_timeval_array is array(Natural range <>) of s_timeval;
    type s_timeval_array2 is array(Natural range 1..2) of s_timeval;
+
+   type u_sockaddr ( Discr : uint16_t := AF_UNSPEC ) is
+      record
+         case Discr is
+            when AF_INET =>
+               addr_in :      s_sockaddr_in;
+            when AF_UNIX =>
+               addr_un :      s_sockaddr_un;
+            when others =>
+               addr_sock :    s_sockaddr;
+         end case;
+      end record;
+
+   pragma Unchecked_Union(u_sockaddr);
+   pragma Convention(C,u_sockaddr);
